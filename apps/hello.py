eb607cb0ca19feb8028b25688bae20a0d97f9135
Ben <ben.menchaca@gmail.com>
2019-02-24 10:26:10 -0600

    Added latest hello.py.

--- apps/hello.py
+++ apps/hello.py
@@ -1,8 +1,12 @@
 import appdaemon.plugins.hass.hassapi as hass
+#
+# Hellow World App
+#
+# Args:
+#
 
 class HelloWorld(hass.Hass):
 
-    def initialize(self):
-        self.log("Hello from Appdaemon")
-        self.log("You are now ready to run apps!")
-
+  def initialize(self):
+     self.log("Hello from AppDaemon")
+     self.log("You are now ready to run Apps!") 
